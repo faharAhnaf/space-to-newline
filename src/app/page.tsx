326a48ebf66770ceed14bcf4f2ea639feed4f7ac
Fahar Ahnaf Azis <faharahnaf66@gmail.com>
2024-10-18 23:24:34 +0700

    feat: Adding toggle mode & contributors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { FloatingNav } from "@/components/ui/floating-navbar";
 import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
 import { useState, useEffect, useRef, ChangeEvent } from "react";
 import { ButtonsCard } from "@/components/ui/tailwindcss-buttons";
+import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function Home() {
   const [text, setText] = useState("");
@@ -24,15 +26,26 @@ export default function Home() {
   //   setText(e.target.value);
   // };
 
+  const handleSpaceToNewline = () => {
+    // e.preventDefault();
+    const updatedText = text.replace(/ /g, "\n");
+    setText(updatedText);
+  };
+
+  const handleNewlineToSpace = () => {
+    // e.preventDefault();
+    const updatedText = text.replace(/\n/g, " ");
+    setText(updatedText);
+  };
+
   return (
     <div className="flex min-h-screen flex-col">
-      <FloatingNav
-        className=""
-        navItems={[
-          { name: "Home", link: "/" },
-          { name: "About", link: "/about" },
-        ]}
-      />
+      <nav className="m-4 flex justify-between">
+        <div>tes</div>
+        <div>
+          <ModeToggle />
+        </div>
+      </nav>
       <HeroHighlight>
         <div>
           <p className="mx-1 text-center text-5xl font-bold leading-snug md:text-6xl">
@@ -40,40 +53,95 @@ export default function Home() {
           </p>
         </div>
       </HeroHighlight>
-      <section className="dark:bg-black flex-grow">
+      <section className="flex-grow dark:bg-black">
         <div className="mx-4 grid space-y-5">
           <textarea
-            className="border-xl mt-10 grid h-40 md:h-56 w-[40vh] mx-auto items-center resize-none rounded-lg border p-3 leading-none dark:bg-white dark:text-black"
+            className="border-xl mx-auto mt-10 grid h-40 w-[40vh] resize-none items-center rounded-lg border-4 p-3 leading-none md:h-56 dark:bg-white dark:text-black"
             name="textarea"
             id="textarea"
             // ref={textAreaRef}
             rows={1}
-            // value={text}
-            // onChange={handleTextareaChange}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
           />
 
-          <div className="mx-auto grid grid-cols-2 space-x-3">
-            <ButtonsCard>
+          <div className="mx-auto grid grid-cols-2 space-x-3 md:space-x-3">
+            <ButtonsCard onClick={handleSpaceToNewline}>
               <button className="relative p-[3px]">
                 <div className="absolute inset-0 rounded-lg bg-gradient-to-r from-indigo-500 to-purple-500" />
-                <div className="group relative rounded-[6px] bg-black px-8 py-2 text-white transition duration-200 hover:bg-transparent">
-                  Newline To Space
+                <div className="group relative rounded-[6px] bg-white px-8 py-2 transition duration-200 hover:bg-transparent hover:text-white dark:bg-black dark:text-white dark:hover:bg-transparent">
+                  Space To Newline
                 </div>
               </button>
             </ButtonsCard>
-            <ButtonsCard>
+            <ButtonsCard onClick={handleNewlineToSpace}>
               <button className="relative p-[3px]">
                 <div className="absolute inset-0 rounded-lg bg-gradient-to-r from-indigo-500 to-purple-500" />
-                <div className="group relative rounded-[6px] bg-black px-8 py-2 text-white transition duration-200 hover:bg-transparent">
-                  Space To Newline
+                <div className="group relative rounded-[6px] bg-white px-8 py-2 transition duration-200 hover:bg-transparent hover:text-white dark:bg-black dark:text-white dark:hover:bg-transparent">
+                  Newline To Space
                 </div>
               </button>
             </ButtonsCard>
           </div>
         </div>
       </section>
-      <footer className="bg-white text-white p-4">
-        <div>your footer here</div>
+      <footer className="dark:bg-black">
+        <div className="flex w-full flex-row items-center justify-center">
+          <div className="m-10 flex mr-14">
+            <AnimatedTooltip
+              items={[
+                {
+                  id: 1,
+                  name: "Fahar Ahnaf Azis",
+                  designation: "10122448",
+                  image: "/images/fahar-ahnaf-azis.jpeg",
+                },
+                {
+                  id: 2,
+                  name: "Christian Andri Siahaan",
+                  designation: "10122297",
+                  image: "/images/christian-andri-siahaan.jpeg",
+                },
+                {
+                  id: 3,
+                  name: "Dylan Edlyano",
+                  designation: "10122410",
+                  image: "/images/dylan-edlyano.jpeg",
+                },
+                {
+                  id: 4,
+                  name: "Bagas Wahyu Andrianto",
+                  designation: "10122264",
+                  image: "/images/bagas-wahyu.jpeg",
+                },
+                {
+                  id: 5,
+                  name: "Fadhil Muhammad",
+                  designation: "10122443",
+                  image: "/images/fadhil-muhammad.jpeg",
+                },
+                {
+                  id: 6,
+                  name: "Sultan Razzan Iza Saloewa",
+                  designation: "11122403",
+                  image: "/images/sultan-razzan.jpeg",
+                },
+                {
+                  id: 7,
+                  name: "Devina Putri Gitani",
+                  designation: "10122361",
+                  image: "/images/devina-putri-gitani.jpeg",
+                },
+                {
+                  id: 8,
+                  name: "Velin Alivia Putri",
+                  designation: "10122011",
+                  image: "/images/velin-alivia.jpeg",
+                },
+              ]}
+            />
+          </div>
+        </div>
       </footer>
     </div>
   );
